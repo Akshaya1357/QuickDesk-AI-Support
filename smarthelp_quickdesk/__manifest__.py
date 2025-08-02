b304f41920324b8b17a022926c7b9424cceb92a7
Akshaya_mary_007 <82075056+Akshaya1357@users.noreply.github.com>
2025-08-02 08:22:26 +0530

    Create __manifest__.py

---
+++ smarthelp_quickdesk/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'SmartHelp QuickDesk',
+    'version': '1.0',
+    'summary': 'AI-powered Helpdesk for Odoo Hackathon',
+    'description': 'QuickDesk with AI reply suggestions, auto-tagging, and summarisation using OpenAI',
+    'author': 'Akshaya Mary Lingala',
+    'category': 'Customer Support',
+    'depends': ['base', 'mail'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
